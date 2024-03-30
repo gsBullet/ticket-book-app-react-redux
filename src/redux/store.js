9567b82200b78978143fc7bc7150aba1281ce549
Golam Sarowar <86582538+gsBullet@users.noreply.github.com>
2024-03-30 20:16:35 +0600

    add logger and custom Logger and middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,19 @@
-import { createStore } from "redux";
+import { createStore , applyMiddleware} from "redux";
 import ticketReducer from "./ticketReducer";
+import logger from "redux-logger";
 
+
+// ******custom logger *******
+// const myLogger = (store)=> (next)=> (action)=>{
+
+//     console.log(action.payload);
+//     console.log(store.getState());
+
+//     return next(action);
+// }
+
+
+// **********middleware *****
+// const store = createStore(ticketReducer,applyMiddleware(logger));
 const store = createStore(ticketReducer);
 export default store;
